bc8769e82594b4e75410b0b4f642d7d8f8d4f32e
Vitali_Vianhura <vengura.code@gmail.com>
2020-09-30 23:26:06 +0300

    feat: provide implementations to the min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+    return reduce(array, (accumulator, value) => value < accumulator ? value : accumulator);
 }
 
 exports.max = function max (array) {
-  return 0;
+    return reduce(array, (accumulator, value) => value > accumulator ? value : accumulator);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  const sum = reduce(array, (accumulator, value) => accumulator + value);
+
+  return array && array.length > 0 ? sum / array.length : 0;
+}
+
+function reduce(array, reducer) {
+    let result = 0;
+    if (array && array.length > 0) {
+        result = array.reduce(reducer, 0);
+    }
+    return result;
 }
